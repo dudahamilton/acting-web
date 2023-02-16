f237a71eb183040a0b7cd537c77304f2de9bf27e
dudahamilton <mariaehamilton28@gmail.com>
2023-02-16 11:36:08 -0800

    footer is generally styled

--- acting-web/components/Footer.js
+++ acting-web/components/Footer.js
@@ -8,7 +8,8 @@ import { SocialIcon } from 'react-social-icons';
 
 export default function footer(){
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.footer}>
             <h3>Contact</h3>
             <h5>DDO Artists Agency <br /> </h5>
                <p> Reprensentatives: <br />
@@ -17,14 +18,23 @@ export default function footer(){
                 +1 323 462 8000 <br />
                 reception@ddoagency.com
                 </p>
-            <div>
+            </div>
+            <div className={styles.footer}>
                 <h3>Social</h3>
+                <div>
                 <SocialIcon url="https://www.instagram.com/ialondradelgado/?hl=en" />
                 <SocialIcon url="https://www.tiktok.com/@ialondradelgado" />
                 <SocialIcon url="https://twitter.com/alondraamariee1?lang=en" />
                 <SocialIcon url="https://www.facebook.com/loly5719" />
+                </div>
+                
+                <div>
+                    <p>© Made by Maria Hamilton with ❤️</p>
+                    <SocialIcon url="https://www.linkedin.com/in/mariahamiltondev/" />
+                </div>    
+            </div>
 
-            <div>
+            <div className={styles.footer}>
                 <h3>Acting Profiles</h3>
                 <a target='_blank' href='https://pro.imdb.com/name/nm4549589/?ref_=recent_view_1'>
                 <Image
@@ -33,15 +43,11 @@ export default function footer(){
                     width='90'
                     height='50'/>
                 </a>
-            </div>
-
-            <div>
-                <p>© Made by Maria Hamilton with ❤️</p>
-                <SocialIcon url="https://www.linkedin.com/in/mariahamiltondev/" />
-            </div>
-            </div>
-                
+            </div>        
         </div>
+         
+
+
         
     )
 }
